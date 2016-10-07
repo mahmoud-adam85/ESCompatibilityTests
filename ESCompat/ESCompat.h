ffa866e63ed1b8e1c583349066cc8e17efe52fc6
Mahmoud Adam <mahmoud@cliqz.com>
2016-10-07 16:51:25 +0200

    Implemented ES5 & ES6 compatibility tests

---
+++ ESCompat/ESCompat.h
@@ -0,0 +1,19 @@
+//
+//  ESCompat.h
+//  ESCompat
+//
+//  Created by Mahmoud Adam on 10/5/16.
+//  Copyright © 2016 cliqz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESCompat.
+FOUNDATION_EXPORT double ESCompatVersionNumber;
+
+//! Project version string for ESCompat.
+FOUNDATION_EXPORT const unsigned char ESCompatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESCompat/PublicHeader.h>
+
+
---
+++ ESCompatTests/ESCompatTests.swift
@@ -0,0 +1,25 @@
+//
+//  ESCompatTests.swift
+//  ESCompatTests
+//
+//  Created by Mahmoud Adam on 10/5/16.
+//  Copyright © 2016 cliqz. All rights reserved.
+//
+
+import XCTest
+@testable import ESCompat
+
+class ESCompatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
